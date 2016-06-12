2e15f69a2ae441f98d1eeb1e23f44e52854ac09f
opsb <oliver@opsb.co.uk>
2016-06-12 20:27:58 +0200

    Avoid using previous requester if possible

--- lib/penelope/slack_utils.ex
+++ lib/penelope/slack_utils.ex
@@ -3,8 +3,25 @@ defmodule Penelope.SlackUtils do
     slack
     |> Map.get(:users)
     |> Map.values
-    |> Enum.filter fn (user) ->
-      user.presence == "active" && user.id != requester_id && !user.is_bot
+    |> Enum.filter(&(elligible?(slack, state, requester_id, &1)))
+    |> remove_previous_reviewer(state)
+  end
+
+  defp elligible?(slack, state, requester_id, user) do
+    Enum.all?([
+      user.presence == "active",
+      user.id != requester_id,
+      !user.is_bot
+    ])
+  end
+
+  defp remove_previous_reviewer(reviewers, state) do
+    previous_reviewer_id = Map.get(state, :previous_reviewer_id)
+
+    if previous_reviewer_id && Enum.count(reviewers) > 1 do
+      Enum.filter(reviewers, & &1.id != previous_reviewer_id)
+    else
+      reviewers
     end
   end
 end
--- test/penelope/slack_utils_test.exs
+++ test/penelope/slack_utils_test.exs
@@ -8,17 +8,33 @@ defmodule Penelope.SlackUtilsTest do
         "opsb_id": %{id: "opsb_id", name: "opsb", presence: "active", is_bot: false},
         "hal_id": %{id: "hal_id", name: "hal", presence: "active", is_bot: true},
         "bob_id": %{id: "bob_id", name: "bob", presence: "active", is_bot: false},
-        "away_id": %{id: "away_id", name: "away", presence: "away", is_bot: false}
+        "away_id": %{id: "away_id", name: "away", presence: "away", is_bot: false},
+        "mary_id": %{id: "mary_id", name: "mary", presence: "active", is_bot: false}
       }
     }
-    state = %{}
+    state = %{previous_reviewer_id: "mary_id"}
     requester_id = "jon_id"
 
     reviewer_ids = Penelope.SlackUtils.find_reviewers(slack, state, requester_id) |> Enum.map(& &1[:id])
 
-    assert EnumUtils.all_members?(reviewer_ids, ["opsb_id", "bob_id"]), "should include all active users"
+    assert EnumUtils.all_members?(reviewer_ids, ["opsb_id", "bob_id"]), "should include active users"
+    assert !Enum.member?(reviewer_ids, "mary_id"), "should exclude previous requester"
     assert !Enum.member?(reviewer_ids, "jon_id"), "should exclude requester"
     assert !Enum.member?(reviewer_ids, "hal_id"), "should exclude bots"
     assert !Enum.member?(reviewer_ids, "away_id"), "should exclude away users"
+  end
+
+  test "find_reviewers when previous reviewer is only person available" do
+    slack = %{
+      users: %{
+        "mary_id": %{id: "mary_id", name: "mary", presence: "active", is_bot: false}
+      }
+    }
+    state = %{previous_reviewer_id: "mary_id"}
+    requester_id = "jon_id"
+
+    reviewer_ids = Penelope.SlackUtils.find_reviewers(slack, state, requester_id) |> Enum.map(& &1[:id])
+
+    assert EnumUtils.all_members?(reviewer_ids, ["mary_id"]), "should include previous reviewer"
   end
 end
